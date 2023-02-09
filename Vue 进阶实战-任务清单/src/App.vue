c3601ea3f4b319d9b0a47551f72058260b47e05b
huodaxia <yougexiaoyuan@qq.com>
2023-02-09 21:27:48 +0800

    更新内容

---
+++ Vue 进阶实战-任务清单/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="有个小院-任务清单"
+      :showAddTask="showAddTask"
+    />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Tasks from './components/Tasks.vue'
+import Footer from './components/Footer'
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Tasks,
+    AddTask
+  },
+  data() {
+    return {
+      showAddTask: false,
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ Vue 进阶实战-任务清单/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+      <p>Copyright &copy; 有个小院</p>
+      <router-link to="/about">2022</router-link>
+    </footer>
+  </template>
+  
+  <style scoped>
+  a {
+    color: #333;
+  }
+  
+  footer {
+    margin-top: 30px;
+    text-align: center;
+  }
+  </style>
+  
